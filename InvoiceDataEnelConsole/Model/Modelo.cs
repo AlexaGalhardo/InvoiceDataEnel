6bd6b3dda92158f01d5575fddda92731f19b8601
Mayara Alves <mayaaa2319@hotmail.comm>
2021-03-30 18:41:56 -0300

    Modelo carregado

---
+++ InvoiceDataEnelConsole/Model/Modelo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoiceDataEnelConsole.Model
+{
+    public class Modelo
+    {
+        public int Posicao { get; set; }
+        public int Cliente { get; set; }
+        public uint Cep { get; set; }
+        public string NmCasa { get; set; }
+        public string Complemento { get; set; }
+        public string Regiao { get; set; }
+        public int Dia { get; set; }
+        public string Mes { get; set; }
+        public int Ano { get; set; }
+        public int Hora { get; set; }
+        public int Minuto { get; set; }
+        public int Segundo { get; set; }
+        public string Medidor { get; set; }
+        public int Aparelho { get; set; }
+        public int Kw { get; set; }
+        public decimal Custo { get; set; }
+
+
+
+    }
+}
