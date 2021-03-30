c7025128f4d6c5c4bd140814d05337f1941e04ed
Sthefany <sthefanycristovam@gmail.com>
2021-03-30 20:56:42 -0300

    definição de pastas, file manager adicionado

--- InvoiceDataEnelConsole/Program.cs
+++ InvoiceDataEnelConsole/Program.cs
@@ -6,32 +6,29 @@ namespace InvoiceDataEnelConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Escreva o caminho da sua fatura: ");
+            string path = Console.ReadLine();
 
-            string registro = "00000348340483300100051Bloco 1A apto 44    ##SSP29Março     20211544230007A16894030000840001550";
-
-            if (registro.Length <= 95)
-            {
-                Model.Modelo model = new Model.Modelo();
-                model.Posicao = 1;
-
-                model.Cliente = Convert.ToInt32(registro.Substring(0, 10).PadLeft(10, '0'));
-                model.Cep = Convert.ToUInt32(registro.Substring(10, 8));
-                model.NmCasa = registro.Substring(18, 5);
-                model.Complemento = registro.Substring(23, 20);
-                model.Regiao = registro.Substring(43, 5);
-                model.Dia = Convert.ToInt32(registro.Substring(48, 2));
-                model.Mes = registro.Substring(50, 10);
-                model.Ano = Convert.ToInt32(registro.Substring(60, 4));
-                model.Hora = Convert.ToInt32(registro.Substring(64, 2));
-                model.Minuto = Convert.ToInt32(registro.Substring(66, 2));
-                model.Segundo = Convert.ToInt32(registro.Substring(68, 2));
-                model.Medidor = registro.Substring(70, 10);
-                model.Aparelho = Convert.ToInt32(registro.Substring(80, 2));
-                model.Kw = Convert.ToInt32(registro.Substring(82, 6));
-                model.Custo = Convert.ToDecimal(registro.Substring(88, 7));
-
-            }
+            List<string> invoices = FileManager(path);
+            InvoiceValidatorManager(invoices);
+            
         }
+
+
+        public List<string> FileManager (string path)
+        {
+            string line;
+            List<string> invoices = new List<string>();
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@path);
+            while ((line = file.ReadLine()) != null)
+            {
+                invoices.Add(line);
+            }
+
+            file.Close();
+            return invoices;
+        }
+
     }
 }
