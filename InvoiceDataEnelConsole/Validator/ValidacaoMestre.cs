3435b1db26496f7f03863674d997455898210414
Mayara Alves <mayaaa2319@hotmail.comm>
2021-03-31 03:11:48 -0300

    Validacao teste

---
+++ InvoiceDataEnelConsole/Validator/ValidacaoMestre.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoiceDataEnelConsole.Validator
+{
+    class ValidacaoMestre
+    {
+        public List<Model.RecordError> Validar(Model.InvoiceData model)
+        {
+            List<Model.RecordError> ListaErros = new List<Model.RecordError>();
+            Model.RecordError Erro= new Model.RecordError();
+
+            if (model.Cliente.ToString().Length != 10)
+            {
+                Erro.Error= " incompleto";
+                Erro.Line = model.Posicao;
+                Erro.Field = "Campo codigo do cliente";
+
+                ListaErros.Add(Erro);
+            }
+
+            if(model.Cep.ToString().Length != 8)
+            {
+                // throw new ArgumentException("");
+            }
+            // if(model.)
+            return ListaErros;
+        }
+    }
+}
