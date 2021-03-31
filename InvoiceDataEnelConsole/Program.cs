d3239edceb2fba7ca8c96ccf8eaa4ff3eca24e56
Alexa Galhardo <Alexa Galhardo@DESKTOP-C94DGC7>
2021-03-31 03:10:34 -0300

    Adicionando zeros

--- InvoiceDataEnelConsole/Program.cs
+++ InvoiceDataEnelConsole/Program.cs
@@ -46,19 +46,19 @@ namespace InvoiceDataEnelConsole
                     model.Posicao =+ 1;
                     model.Cliente = Convert.ToInt32(invoice.Substring(0, 10).PadLeft(10, '0'));
                     model.Cep = Convert.ToUInt32(invoice.Substring(10, 8));
-                    model.NmCasa = invoice.Substring(18, 5);
-                    model.Complemento = invoice.Substring(23, 20);
-                    model.Regiao = invoice.Substring(43, 5);
+                    model.NmCasa = invoice.Substring(18, 5).PadLeft(5, '0');
+                    model.Complemento = invoice.Substring(23, 20).PadRight(20, ' ');
+                    model.Regiao = invoice.Substring(43, 5).PadLeft(5, '#');
                     model.Dia = Convert.ToInt32(invoice.Substring(48, 2));
-                    model.Mes = invoice.Substring(50, 10);
+                    model.Mes = invoice.Substring(50, 10).PadRight(10, ' ');
                     model.Ano = Convert.ToInt32(invoice.Substring(60, 4));
-                    model.Hora = Convert.ToInt32(invoice.Substring(64, 2));
-                    model.Minuto = Convert.ToInt32(invoice.Substring(66, 2));
-                    model.Segundo = Convert.ToInt32(invoice.Substring(68, 2));
-                    model.Medidor = invoice.Substring(70, 10);
+                    model.Hora = Convert.ToInt32(invoice.Substring(64, 2).PadLeft(2, '0'));
+                    model.Minuto = Convert.ToInt32(invoice.Substring(66, 2).PadLeft(2, '0'));
+                    model.Segundo = Convert.ToInt32(invoice.Substring(68, 2).PadLeft(2, '0'));
+                    model.Medidor = invoice.Substring(70, 10).PadLeft(2, '0');
                     model.Aparelho = Convert.ToInt32(invoice.Substring(80, 2));
-                    model.Kw = Convert.ToInt32(invoice.Substring(82, 6));
-                    model.Custo = Convert.ToDecimal(invoice.Substring(88, 7));
+                    model.Kw = Convert.ToInt32(invoice.Substring(82, 6).PadLeft(6, '0')) ;
+                    model.Custo = Convert.ToDecimal(invoice.Substring(88, 7).PadLeft(7, '0'));
 
                     lista.Add(model);
                 }
